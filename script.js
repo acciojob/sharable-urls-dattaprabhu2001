f9b2302a7b3e873e537215ab5c82ec86091b8eab
dattaprabhu2001 <99395413+dattaprabhu2001@users.noreply.github.com>
2023-02-26 06:45:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // your code here
+var form=document.querySelector("form");
+var nameValue=document.querySelector("#name");
+var yearValue=document.querySelector("#year");
+var btn=document.querySelector("#button");
+var h3=document.querySelector("h3");
+ function handleSubmit(event){
+	 event.preventDefault();
+	 var h3value="https://localhost:8080/";
+	 var name=nameValue.value;
+	 var year=yearValue.value;
+
+	 if(name && year){
+		 h3value+="?name="+name=+"&year="+year;
+	 }else if(name && !year){
+		 h3value+="?name="+name;
+	 }else if(!name && year){
+		 h3value+="?year="+year;
+	 }
+	 h3.textContent=h3value;
+ }
+form.addEventListener("submit",handleSubmit)
